9497a148a12c7279d662247f9de8b885984d3bee
isabelaescalante <isaescalante123@gmail.com>
2019-03-21 09:40:25 -0600

    Semantic cube with verification

---
+++ Chapter3/cube.py
@@ -0,0 +1,45 @@
+class Cube:
+    def __init__(self):
+        self.cube = {}
+
+        bools = {}
+        numbers = {}
+        words = {}
+        sentences = {}
+
+        # se pone la negacion de bools?
+        bools['operator_assign'] = 'bool'
+        bools['operator_and'] = 'bool'
+        bools['operator_or'] = 'bool'
+
+        numbers['operator_assign'] = 'number'
+        numbers['operator_add'] = 'number'
+        numbers['operator_minus'] = 'number'
+        numbers['operator_mult'] = 'number'
+        numbers['operator_div'] = 'number'
+        numbers['operator_greater'] = 'number'
+        numbers['operator_less'] = 'number'
+        numbers['operator_equal'] = 'number'
+        numbers['operator_notequal'] = 'number'
+
+
+        words['operator_assign'] = 'word'
+        words['operator_add'] = 'sentence'
+
+        sentences['operator_assign'] = 'sentence'
+
+        self.cube['bool'] = bools
+        self.cube['number'] = numbers
+        self.cube['word'] = words
+        self.cube['sentence'] = sentences
+
+    def verification(self, operator, data_type1, data_type2):
+        types = ['bool','number','word','sentence']
+
+        if data_type1 in types and data_type1 == data_type2:
+            if operator in self.cube[data_type1]:
+                return self.cube[data_type1][operator]
+            else:
+                return None
+        else:
+            return None
