43af4b0edc564d1ad6eee838521bf7a34d85d8d7
isabelaescalante <isaescalante123@gmail.com>
2019-05-07 23:04:57 -0500

    Code is uploaded with file

--- Python/main.py
+++ Python/main.py
@@ -21,30 +21,14 @@ def getInputs() :
 
 
 if __name__ == '__main__':
-    code = '''kotoba program1;
-    
-          declare number x, number f;
-          function number fib(number n) {
-              declare number a, number aAux, number b, number bAux;
-               if(n < 2.0){
-                   return n;
-               }else{
-                   set aAux = n - 1.0;
-                   set bAux = n - 2.0;
-                   set a = call fib(aAux);
-                   set b = call fib(bAux);
-                   return (a + b);
-               }
-           }
-          begin
-          {
-              set x = 7.0;
-              set f = call fib(x);
-              kprint(f);
-          }
-          end
-  '''
+    code = ''
 
+    fileCode = open("code.txt","r")
+
+    if fileCode.mode == 'r':
+        code = fileCode.read()
+
+    fileCode.close()
 
     parseCode(code)
     variables = getFinalVariables()
